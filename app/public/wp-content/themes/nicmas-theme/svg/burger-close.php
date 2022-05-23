750ac1a15ee0290a3c40148d5d5e81dc1dcc39db
Lozya <vlad.lozitskiy@gmail.com>
2022-05-23 22:02:16 +0300

    add mobile header

---
+++ app/public/wp-content/themes/nicmas-theme/svg/burger-close.php
@@ -0,0 +1,4 @@
+<svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1.12891 21.8257L22.054 0.900562" stroke="white" stroke-width="2"/>
+    <path d="M1.12891 1.28076L22.054 22.2059" stroke="white" stroke-width="2"/>
+</svg>
